6c37765f71671122fc899890fbe9b1d3368206c4
masahiro sone <masahiro_sone@mediado.jp>
2022-08-30 21:54:02 +0900

    add POP/RPC settings

--- src/index.js
+++ src/index.js
@@ -6,8 +6,23 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 fcl.config()
-  .put("accessNode.api", "https://access-testnet.onflow.org") // connect to Flow testnet
-  .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn") // use Blocto testnet wallet
+  // master origin
+  // .put("accessNode.api", "https://access-testnet.onflow.org") // connect to Flow testnet
+  // .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn") // use Blocto testnet wallet
+
+  // ref) https://docs.blocto.app/blocto-sdk/javascript-sdk/flow/login-register
+  // POP/RPC
+  .put("accessNode.api", "https://rest-testnet.onflow.org")
+  .put("discovery.wallet.method", "POP/RPC")
+  .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn")
+    
+  // HTTP/POST
+  // .put("accessNode.api", "https://rest-testnet.onflow.org")
+  // .put(
+  //   "discovery.wallet",
+  //   "https://flow-wallet-testnet.blocto.app/api/flow/authn"
+  // )
+  // .put("discovery.wallet.method", "HTTP/POST")
 
 ReactDOM.render(
   <React.StrictMode>
